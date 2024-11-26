d243b970c56e5514a34285b68552a8d204badbdc
Lucas Araujo <dcoderltda@gmail.com>
2024-11-25 23:29:35 -0300

    Alterações para adicçãodas demais áreas da pagina parametrizada

--- funcoes.js
+++ funcoes.js
@@ -1,4 +1,6 @@
 let menulist = document.getElementById("menuList")
+const btnWpp = document.querySelector('#btn-Wpp')
+
 menulist.style.maxHeight = "0px";
 
 function toggleMenu() {
@@ -8,3 +10,15 @@ function toggleMenu() {
         menulist.style.maxHeight = "0px"
     }
 }
+
+
+function SendWppMessage() {
+    let DestNumber = '5585996604247';
+    let mensagem = 'Olá, gostaria de saber mais sobre seus serviços de desenvolvimento web. Estou interessado em criar uma página profissional e gostaria de conversar sobre detalhes e orçamento. Aguardo seu retorno!';
+    // let WppUrl = 'https://wa.me/' + DestNumber + '?/text=' + encodeURIComponent(mensagem);
+    let WppUrl = 'https://api.whatsapp.com/send?phone=' + DestNumber + '&text=' + encodeURIComponent(mensagem);
+    // console.log(WppUrl)
+    window.open(WppUrl, '_blank')
+}
+
+btnWpp.addEventListener("click", SendWppMessage)
